d5f772f61756082a438abb5fdec610f43afe9add
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-12-09 19:20:50 +0100

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express     = require('express')
 const app         = express()
 const verifyToken = require('./middlewares/auth-middleware')
-
+const cors        = require('cors')
 
 app.use(express.json())
-
+app.use(cors())
 const PORT = process.env.PORT || 3000
 
 
